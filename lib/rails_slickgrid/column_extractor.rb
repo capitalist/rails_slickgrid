b5fc396dfde13fbcc49d595f39286e9c18c5545d
Joe Martinez <joe@joemartinez.name>
2011-01-23 20:09:02 -0500

    fix bug with col extractor

--- lib/rails_slickgrid/column_extractor.rb
+++ lib/rails_slickgrid/column_extractor.rb
@@ -2,8 +2,21 @@ module RailsSlickgrid
   module ColumnExtractor
     extend self
     def from_data(data)
+      return from_hash_keys(data) if data.first.is_a? Hash
+      return from_ar_columns(data) if data.first.is_a? ActiveRecord::Base
+      return data
+    end
+
+    def from_hash_keys data
       data.first.keys.collect do |kee|
 	{:id => kee, :name => kee.to_s.humanize, :field => kee}
+      end
+    end
+
+    def from_ar_columns data
+      # TODO: add size info
+      data.first.class.content_columns.collect do |col|
+	{:id => col.name, :name => col.name.to_s.humanize, :field => col.name}
       end
     end
   end
